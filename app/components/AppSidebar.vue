b9692fe9dbd07ade6ac78795f19389076ad98aed
wallcreation <lanhajames@gmail.com>
2026-04-30 15:01:11 +0100

    Add greeting function and update appside bar

--- app/components/AppSidebar.vue
+++ app/components/AppSidebar.vue
@@ -2,15 +2,38 @@
   <UDashboardSidebar
     collapsible
     resizable
-    :ui="{ footer: 'border-t border-default' }"
+    :ui="{ footer: 'border-t border-default', root: 'hidden md:flex flex-col' }"
   >
     <template #header="{ collapsed }">
-      <h1 v-if="!collapsed" class="pl-2 text-5xl font-bold">Flow</h1>
-      <UIcon
-        v-else
-        name="i-simple-icons-nuxtdotjs"
-        class="size-5 text-primary mx-auto"
-      />
+      <div class="w-full flex flex-col items-center gap-2 py-2">
+        <div
+          class="flex w-full items-center"
+          :class="collapsed ? 'justify-center' : 'justify-between'"
+        >
+          <h1 v-if="!collapsed" class="pl-2 text-5xl font-bold">Flow</h1>
+          <UIcon
+            v-else
+            name="i-simple-icons-nuxtdotjs"
+            class="size-5 text-primary"
+          />
+
+          <UDashboardSidebarCollapse
+            v-if="!collapsed"
+            variant="ghost"
+            color="neutral"
+            icon="i-lucide-chevron-left"
+          />
+        </div>
+
+        <!-- Bouton en bas quand fermé -->
+        <UDashboardSidebarCollapse
+          v-if="collapsed"
+          variant="ghost"
+          color="neutral"
+          class="mt-auto"
+          icon="i-lucide-chevron-right"
+        />
+      </div>
     </template>
 
     <template #toggle>
