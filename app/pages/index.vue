09cec2c3cfe3abbfb08c55e13f7d966edb3b9fe2
wallcreation <lanhajames@gmail.com>
2026-04-30 15:08:01 +0100

    Setup base home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="">hello</div>
+  <div class="px-2">
+    <UPageHeader
+      :title="title"
+      description="Prêt à créer quelque chose d'incroyable aujourd'hui ?"
+      :ui="{ root: 'relative py-2 border-none' }"
+    />
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const title = gretting() + ", User";
+</script>
 
 <style></style>
