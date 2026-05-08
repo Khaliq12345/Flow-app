ee98115b0017afa8f118c82697ee0fcaf18c965b
wallcreation <lanhajames@gmail.com>
2026-05-08 10:38:13 +0100

    Add chillaw police and update header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -4,21 +4,22 @@
     class="w-full flex items-center justify-between gap-4 flex-wrap py-4 border-b-2 border-gray-500"
   >
     <div class="space-y-2">
-      <h1
-        class="text-xl sm:text-4xl font-medium text-gray-900 dark:text-white leading-tight"
-      >
-        {{ title }}
-      </h1>
+      <div class="flex items-center gap-2">
+        <UButton
+          variant="ghost"
+          color="neutral"
+          icon="i-lucide-arrow-left"
+          @click="$router.back()"
+        />
+        <h1
+          class="text-xl sm:text-4xl font-medium text-gray-900 dark:text-white leading-tight"
+        >
+          {{ title }}
+        </h1>
+      </div>
       <p v-if="description" class="text-sm text-gray-500 dark:text-gray-400">
         {{ description }}
       </p>
-      <UButton
-        label="Retour"
-        variant="outline"
-        color="neutral"
-        icon="i-lucide-arrow-left"
-        @click="$router.back()"
-      />
     </div>
 
     <div v-if="$slots.actions" class="flex items-center gap-2 shrink-0">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,7 @@ export default defineNuxtConfig({
       directusUrl: "",
     },
   },
+  fonts: {
+    provider: "google",
+  },
 });
